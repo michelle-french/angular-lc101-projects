507da12ffcd5fb2ee5f4bda1981bec7c738076d4
michelle-french <80438737+michelle-french@users.noreply.github.com>
2021-06-26 01:46:05 -0500

    stash

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   finishedChores = [];
+   chores = ['Complete LaunchCode prep work'];
+   finishedChores = ['Empty dishwasher', 'Buy groceries'];
 
    targetImage = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
 
@@ -17,3 +17,15 @@ export class ChoresComponent implements OnInit {
    }
 
 }
+
+
+// If the chores array is empty OR the finishedChores array has at least 3 more items than the 
+// chores array, display targetImage under the Done Chores list. Otherwise, use a p tag to display 
+// the text, "No allowance yet."// 
+
+// Return to chores.component.ts and change the number of items in 
+// the arrays again. Check to make sure the web page correctly responds to your changes.
+
+// Finally, if the chores array is empty AND finishedChores contains 4 or more items, display 
+// an h1 underneath the lists with the text "Ice cream treat!" Otherwise, display h3 and p elements 
+// that describe how to earn ice cream.
