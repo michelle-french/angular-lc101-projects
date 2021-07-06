4f75eb50fc1ae406d72d475c1eb50d7ac6713e5b
michelle-french <80438737+michelle-french@users.noreply.github.com>
2021-07-06 00:27:04 -0500

    finished

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
    inactive: boolean = false;
+   active: boolean = false;
+   itsAGo: boolean = false;
 
    constructor() { }
 
